bbfa104ec2aed86e6e364eb4ce44f0e98bf15ace
Ivan Morales <ivann_mg@hotmail.com>
2021-04-19 20:16:04 -0500

    Implemente collapsed menu

--- src/components/Shared/Sidebar/Sidebar.tsx
+++ src/components/Shared/Sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
-import { Drawer, Divider, List, ListItem, ListItemText, ListItemIcon } from "@material-ui/core";
+import { Drawer, Divider, List, ListItem, ListItemText, ListItemIcon, Collapse } from "@material-ui/core";
 import HomeIcon from "@material-ui/icons/Home";
 import CreditCardIcon from "@material-ui/icons/CreditCard";
 import ScannerIcon from "@material-ui/icons/Scanner";
 import HelpIcon from "@material-ui/icons/Help";
 import NoteIcon from "@material-ui/icons/Note";
+import { useHistory } from "react-router-dom";
 import "./styles.scss";
+import React from "react";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -12,6 +14,17 @@ interface SidebarProps {
 }
 
 const Sidebar = (props: SidebarProps) => {
+  const history = useHistory();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandMenu = () => {
+    setExpanded(!expanded);
+  };
+
+  function redirect(href: string) {
+    history.push(href);
+  }
+
   return (
     <div>
       <Drawer variant="temporary" anchor="left" open={props.isOpen} ModalProps={{ onBackdropClick: props.onClose }}>
@@ -22,31 +35,42 @@ const Sidebar = (props: SidebarProps) => {
         </div>
         <Divider />
         <List>
-          <ListItem button key={"Inicio"}>
+          <ListItem button key={"Inicio"} onClick={() => redirect("/")}>
             <ListItemIcon>
               <HomeIcon />
             </ListItemIcon>
             <ListItemText primary={"Inicio"} />
           </ListItem>
-          <ListItem button key={"Tarjeta Liverpool"}>
+          <ListItem button key={"Tarjeta Liverpool"} onClick={() => redirect("/beneficios")}>
             <ListItemIcon>
               <CreditCardIcon />
             </ListItemIcon>
             <ListItemText primary={"Tarjeta Liverpool"} />
           </ListItem>
-          <ListItem button key={"Escanea"}>
+          <ListItem button key={"Escanea"} onClick={() => redirect("/scan")}>
             <ListItemIcon>
               <ScannerIcon />
             </ListItemIcon>
             <ListItemText primary={"Escanea"} />
           </ListItem>
-          <ListItem button key={"Departamentos"}>
+          <Divider />
+
+          <ListItem button key={"Departamentos"} onClick={handleExpandMenu}>
             <ListItemIcon>
               <NoteIcon />
             </ListItemIcon>
             <ListItemText primary={"Departamentos"} />
           </ListItem>
-          <ListItem button key={"Ayuda"}>
+          <Divider />
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            {["Zapatería", "Videojuegos", "Computación"].map((text, index) => (
+              <ListItem button key={index} onClick={() => redirect(`/${text}`)}>
+                <ListItemIcon></ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </Collapse>
+          <ListItem button key={"Ayuda"} onClick={() => redirect("/ayuda")}>
             <ListItemIcon>
               <HelpIcon />
             </ListItemIcon>
